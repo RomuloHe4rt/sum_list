72bcb766418586f396b56fdaeab9d6a85f493964
Rômulo Silva <romulo_lss@hotmail.com>
2021-09-18 14:03:20 -0300

    ♻ refac: Changed to tail call optimization

--- lib/sum_list.ex
+++ lib/sum_list.ex
@@ -1,12 +1,11 @@
 defmodule SumList do
-  def call(list), do: sum(list, 0)
+  def call(list), do: sum(list)
 
   def call_enum(list), do: Enum.any?(list, fn elem -> elem > 5 end)
 
-  defp sum([], acc), do: acc
+  defp sum([]), do: 0
 
-  defp sum([head | tail], acc) do
-    acc = acc + head
-    sum(tail, acc)
+  defp sum([head | tail]) do
+    head + sum(tail)
   end
 end
